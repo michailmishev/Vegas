664d5ed84e8bf40d7cf2258560e31a2dcfb2a715
Michail Mishev <m.mishev1@gmail.com>
2017-12-10 21:36:34 +0200

    Initital Commit

---
+++ Vegas/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Vegas
+//
+//  Created by Michail Mishev on 10/12/17.
+//  Copyright © 2017 Michail Mishev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
